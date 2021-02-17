0641f24d40c231e0cf193c45d168f8397c950e58
ayane93sese <koro.115ayapi@gmail.com>
2021-02-17 15:41:29 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,16 @@ class PostsController < ApplicationController
     #メモの保存後にトップページにリダイレクトされる記述
   end
 
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: {post: item}
+  end
+
 end
