e4680d571b423cd45736d315238dadebe7f0b4be
ayane93sese <koro.115ayapi@gmail.com>
2021-02-17 14:16:17 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,15 @@
 class PostsController < ApplicationController
   #ルーティングでposts#indexを定義しているので、postsコントローラーってのを作りましたということですね
   def index #indexという名前のインスタンスメソッドを定義しただけです
-    @posts = Post.all #これをpostsディレクトリのindex.html.erbっていう対応したビューで使えるようになりますって話です
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC") #これをpostsディレクトリのindex.html.erbっていう対応したビューで使えるようになりますって話です
+    #orderメソッドで並び順を変えられる
   end
 
   def create
     Post.create(content: params[:content])
     #これはPostモデルにActiveRecordメソッドのcreateを適用して、その後(カラム名:paramsの中身取り出したいのでparams[:content])と記載してますよ〜
+    redirect_to action: :index
+    #メモの保存後にトップページにリダイレクトされる記述
   end
 
 end
